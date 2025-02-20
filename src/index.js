07f17b0b99a8b978948b9a6cb6ff67ecd4028bb2
KlochkovaIrina <109291169+KlochkovaIrina@users.noreply.github.com>
2025-02-20 21:39:44 +0300

    complete setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import "../index.css";
+import Icon from "../assets/js.png";
 console.log("Hello World!");
+
+const imgTest = document.createElement("img");
+imgTest.src = Icon;
+document.body.append(imgTest);
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 module.exports = {
   mode: "development",
@@ -6,5 +7,38 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "index.html"),
+    }),
+  ],
+  devServer: {
+    static: "./dist",
+    port: 8080,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            targets: "defaults",
+            presets: [["@babel/preset-env"]],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
   },
 };
